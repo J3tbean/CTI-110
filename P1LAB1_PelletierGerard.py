27d570b5ba3d2a79734d79063f1660891fb54028
Zombie <145418835+J3tbean@users.noreply.github.com>
2025-02-06 10:19:32 -0500

    Add files via upload

---
+++ P1LAB1_PelletierGerard.py
@@ -0,0 +1,13 @@
+# Gerard Pelletier
+# 2/4/2025
+# P1LAB1 Instructions
+# A beginners assignment for python, using the input function to have the user enter their name to display in a print statement.
+
+# User inputs for first and last name
+
+user_first_name = input("Enter your first name: ")
+user_last_name = input("Enter your last name: ")
+
+# Print statement to display the results of the prompt
+
+print("Hello,", user_first_name, user_last_name+ "! Welcome to CTI-110!")
